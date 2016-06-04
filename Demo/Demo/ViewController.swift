aa161316636a0d40fba63f137a70a8074edd7197
Kenta Kudo <kentakudo@USER-no-MacBook-Air.local>
2016-06-04 13:36:41 +0900

    first commit

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Kenta Kudo on 2016/06/04.
+//  Copyright © 2016年 KentaKudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PreludeSwift/PreludeSwift.h
@@ -0,0 +1,19 @@
+//
+//  PreludeSwift.h
+//  PreludeSwift
+//
+//  Created by Kenta Kudo on 2016/06/04.
+//  Copyright © 2016年 KentaKudo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PreludeSwift.
+FOUNDATION_EXPORT double PreludeSwiftVersionNumber;
+
+//! Project version string for PreludeSwift.
+FOUNDATION_EXPORT const unsigned char PreludeSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PreludeSwift/PublicHeader.h>
+
+
